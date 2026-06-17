0fcfe15c46d3db0ee847e9d856243c4891dd25f6
VISHAL <sawantvishal2001@gmail.com>
2026-06-17 18:27:25 +0530

    another one

--- Arrays/RemoveElement.java
+++ Arrays/RemoveElement.java
@@ -12,3 +12,18 @@ public class RemoveElement {
         return k;
     }
 }
+
+// class MainClass {
+//     public static void main(String args[]) {
+//         int nums[] = {0,1,2,2,3,0,4,2};
+//         int val = 2;
+//         int k = 0;
+//         for (int i = 0;i<nums.length;i++) {
+//             if (nums[i] != val) {
+//                 nums[k] = nums[i];
+//                 k++;
+//             }
+//         }
+//         System.out.print(k);
+//     }
+// }
