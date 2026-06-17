53686eb0df120b4f481acc2e1826fe836e02fbfc
VISHAL <sawantvishal2001@gmail.com>
2026-06-17 18:15:52 +0530

    array code

---
+++ Arrays/RemoveDuplicate.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+public class RemoveDuplicate {
+    public int removeDuplicates(int[] nums) {
+       int k = 1;
+       for (int i = 1;i<nums.length;i++) {
+        if (nums[i]!=nums[i-1]) {
+            nums[k] = nums[i];
+            k++;
+        }
+       }
+       return k;
+    }
+}
+
+// class Solution {
+//     public int removeDuplicates(int[] nums) {
+//        int k = 1;
+//        for (int i = 1;i<nums.length;i++) {
+//         if (nums[i]!=nums[i-1]) {
+//             nums[k] = nums[i];
+//             k++;
+//         }
+//        }
+//        return k;
+//     }
+// }
