899ca4e405d6aa817546dfa9206262700839c37e
Parag Kulkarni <parag@cosmino.space>
2026-07-19 12:51:58 +0530

    array code

---
+++ Arrays/PlusOne.java
@@ -0,0 +1,35 @@
+class plusOne {
+    public int[] plusOne(int[] digits) {
+        int len = digits.length;
+        for (int i = len - 1;i >= 0;i--) {
+            if (digits[i]<9) {
+                digits[i] = digits[i] + 1;
+                return digits;
+            } else {
+                digits[i] = 0;
+            }
+        }
+        int[] result = new int[len + 1];
+        result[0] = 1;
+        return result;
+    }
+}
+
+
+// import java.util.Arrays;
+
+// class MainClass {
+//     public static void main(String args[]) {
+//         int arr[] = {9};
+//         int len = arr.length;
+//         for (int i = len -1;i>=0;i--) {
+//             if (arr[i]<9) {
+//                 arr[i] = arr[i]+1;
+//                 break;
+//             } else {
+//                 arr[i] = 0;
+//             }
+//         }
+//         System.out.print(Arrays.toString(arr));
+//     }
+// }
