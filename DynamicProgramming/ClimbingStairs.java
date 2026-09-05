47fae02fc007d410d5d3d2869be9c3909fbd7ebc
Vishal Sawant <sawantvishal2001@gmail.com>
2026-09-05 12:01:23 +0530

    update ClimbingStairs.java

--- DynamicProgramming/ClimbingStairs.java
+++ DynamicProgramming/ClimbingStairs.java
@@ -18,3 +18,23 @@ class ClimbingStairs {
     System.out.println(one);
     }}
 }
+
+// LeetCode
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return n;
+        }
+        if (n ==2) {
+            return n;
+        }
+         int one = 1;
+         int two = 2;
+         for (int i = 3; i<=n; i++) {
+            int current = one + two;
+            one = two;
+            two = current;
+         }
+         return two;
+    }
+}
